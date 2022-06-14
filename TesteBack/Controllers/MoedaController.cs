2a0339f4ec335d5284071032e18382baf50c9463
Matheus-Sleutjes <83664192+Matheus-Sleutjes@users.noreply.github.com>
2022-06-14 17:13:21 -0400

    Primeiro Commit

---
+++ TesteBack/Controllers/MoedaController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TesteBack.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MoedaController : ControllerBase
+    {
+
+        public MoedaController()
+        {
+        }
+
+        [HttpGet("GetItemFila")]
+        public void GetItemFila()
+        {
+            
+        }
+
+        [HttpPost("AddItemFila")]
+        public void AddItemFila()
+        {
+
+        }
+    }
+}
